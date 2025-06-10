c73fb6bc107b93b13cae4bf28f0f17e26196ff11
hoetaek <hoetaek@student.snue.ac.kr>
2025-06-10 22:00:49 +0900

    Revert "Fix router base path configuration for GitHub Pages subdirectory"
    This reverts commit 3b9739b9cfe50a95c153be8253de6560849a9d45.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,17 +6,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
-// Get the base path from the current location
-const basePath = import.meta.env.BASE_URL || "/";
-
-function AppRouter() {
+function Router() {
   return (
-    <Router base={basePath}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
@@ -25,7 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <AppRouter />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
